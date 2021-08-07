e1bcece357a19be8fbe0048dbeeca60650e43564
Awad Sandal <awad619@hotmail.com>
2021-08-07 14:10:41 +0300

    Initial Commit

---
+++ Aqarak/AqarakApp.swift
@@ -0,0 +1,17 @@
+//
+//  AqarakApp.swift
+//  Aqarak
+//
+//  Created by Sandal on 28/12/1442 AH.
+//
+
+import SwiftUI
+
+@main
+struct AqarakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
